434f5a98d9db4778b306f799df1928c601123ec4
mmitea <marius.mitea@cae.com>
2022-07-21 14:04:38 -0400

    Add project files.

---
+++ font_test/Program.cs
@@ -0,0 +1,7 @@
+﻿using SixLabors.Fonts;
+
+var fontCollection = new FontCollection();
+var regularFontFamily = fontCollection.Add(@"Font\OpenSans-Regular.ttf");
+var font = regularFontFamily.CreateFont(25);
+
+var fontRect = TextMeasurer.Measure("V", new(font));
