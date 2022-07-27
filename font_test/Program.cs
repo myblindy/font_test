c3d2c9c62ef82bf1548bc3c8a3b4b5900c4f98b7
mmitea <marius.mitea@cae.com>
2022-07-27 11:48:49 -0400

    updates

--- font_test/Program.cs
+++ font_test/Program.cs
@@ -1,7 +1,40 @@
 ﻿using SixLabors.Fonts;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Drawing.Processing;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using System.Diagnostics;
+using System.Drawing;
 
-var fontCollection = new FontCollection();
-var regularFontFamily = fontCollection.Add(@"Font\OpenSans-Regular.ttf");
-var font = regularFontFamily.CreateFont(25);
+var img = new Image<Bgra32>(100, 100);
+img.Mutate(ctx => ctx.DrawText(
+    new DrawingOptions
+    {
+        GraphicsOptions =
+        {
+            //AlphaCompositionMode = PixelAlphaCompositionMode.SrcOver,
+            //ColorBlendingMode= PixelColorBlendingMode.Normal,
+            //AntialiasSubpixelDepth = 1,
+            //Antialias = false
+        }
+    },
+    new TextOptions(SixLabors.Fonts.SystemFonts.CreateFont("Segoe UI", 10))
+    {
+        KerningMode = KerningMode.Normal,
+        ColorFontSupport = ColorFontSupport.MicrosoftColrFormat,
+        HintingMode = HintingMode.HintXY,
+    }, "Assigning food to tree.", SixLabors.ImageSharp.Drawing.Processing.Brushes.Solid(SixLabors.ImageSharp.Color.White), null));
+img.SaveAsPng(@"c:\temp\a.png");
+Process.Start(new ProcessStartInfo(@"c:\temp\a.png") { UseShellExecute = true });
 
-var fontRect = TextMeasurer.Measure("V", new(font));
+var img2 = new Bitmap(100, 100, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+using (var g = Graphics.FromImage(img2))
+{
+    g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+    g.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+    g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+    g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+    g.DrawString("Assigning food to tree.", new System.Drawing.Font("Segoe UI", 10, GraphicsUnit.Pixel), System.Drawing.Brushes.White, new System.Drawing.PointF());
+}
+img2.Save(@"c:\temp\a2.png");
+Process.Start(new ProcessStartInfo(@"c:\temp\a2.png") { UseShellExecute = true });
